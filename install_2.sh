2bd8ef6ff32136e93f8c4bebdcf234f17076e97a
Vrishabh-D-B <bansodvrishabh@pm.me>
2022-12-03 12:44:06 +0530

    03.12.22 12:44

--- install_2.sh
+++ install_2.sh
@@ -1,9 +1,3 @@
-# install bind9 dns server 
-apt update && apt install bind9 bind9utils bind9-doc
-
-# setting network protocol to ipv4
-mv named /etc/default/
-
 # restarting bind9
 systemctl restart bind9
 
