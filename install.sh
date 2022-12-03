7ad75b8c34f4d61fa0d5df6da48ae1888a2177dd
Vrishabh-D-B <bansodvrishabh@pm.me>
2022-12-03 13:15:48 +0530

    03.12.22 01:15

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 # install bind9 dns server 
 apt update && apt install bind9 bind9utils bind9-doc
+process_id=$!
+wait $process_id
 
 # setting network protocol to ipv4
 mv named /etc/default/
+process_id=$!
+wait $process_id
 
 # restarting bind9
 systemctl restart bind9
+process_id=$!
+wait $process_id
 
 # setting up forwarders
 mv named.conf.options /etc/bind/
+process_id=$!
+wait $process_id
 
 # restarting bind9
 systemctl restart bind9
+process_id=$!
+wait $process_id
